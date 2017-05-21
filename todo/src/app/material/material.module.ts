fc9363680c8d4eafc8a669ff466220feb7f79b11
psokolov <psokolov@payneteasy.com>
2017-05-21 11:35:23 +0300

    added MdListModule

--- todo/src/app/material/material.module.ts
+++ todo/src/app/material/material.module.ts
@@ -1,16 +1,18 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import {NoopAnimationsModule} from '@angular/platform-browser/animations';
-import { MdGridListModule } from '@angular/material';
+import { MdGridListModule, MdListModule } from '@angular/material';
 
 @NgModule({
   imports: [
     CommonModule,
     MdGridListModule,
+    MdListModule,
     NoopAnimationsModule
   ],
   exports: [
     MdGridListModule,
+    MdListModule,
     NoopAnimationsModule
   ],
   declarations: []

81aef915273eb54986e929cad433fd4a5d9a0f96
psokolov <psokolov@payneteasy.com>
2017-06-12 21:24:26 +0300

    removed unused code

--- todo/src/app/common/services/storage.service.ts
+++ todo/src/app/common/services/storage.service.ts
@@ -1,4 +1,4 @@
-import { EventEmitter, Injectable, Output } from '@angular/core';
+import { Injectable } from '@angular/core';
 
 @Injectable()
 export class StorageService {
--- todo/types/grid/index.d.ts
+++
@@ -1,6 +0,0 @@
-type grid = {
-  'text': string,
-  'cols': number,
-  'rows': number,
-  'color': string
-};
